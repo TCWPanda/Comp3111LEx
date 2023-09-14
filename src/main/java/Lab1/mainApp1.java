20597e97654f6e50f00c8304954960db5baf6900
bb482 <bb5223913@gmail.com>
2023-09-14 20:53:01 +0800

    Add 2 print lines as report heading This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class mainApp1 {
+    public static void main(String[] args) {
+        int b=2;
+        int n=11;
+        System.out.println(b+"  to power "+n+" = "+myLibrary.Power(b,n));
+        System.out.println(n+"! = "+ myLibrary.factorial(n));
+        System.out.println("Program ..Ended ...");
+    }
+}
